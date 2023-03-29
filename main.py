53db2132c6d496750db28e098561c0d3c13df994
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-29 16:33:15 -0700

    added api

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
+import requests
+import connect
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
+# STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-## STEP 2: Use https://newsapi.org
+parameters = {
+    "function": "TIME_SERIES_INTRADAY",
+    "symbol": STOCK,
+    "interval": "60min",
+    "apikey": connect.API_KEY
+}
+
+url = 'https://www.alphavantage.co/query'
+r = requests.get(url, params=parameters)
+data = r.json()
+
+
+
+print(data["Time Series (60min)"]["2023-03-28 18:00:00"]["4. close"])
+print(data["Time Series (60min)"]["2023-03-27 18:00:00"]["4. close"])
+
+# STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# STEP 3: Use https://www.twilio.com
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to 
+file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height 
+of the coronavirus market crash.
 or
 "TSLA: 🔻5%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to 
+file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height 
+of the coronavirus market crash.
 """
-
