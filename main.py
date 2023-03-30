883ce131ada3b65b8c887341aaa41f464fefa86d
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-30 15:55:12 -0700

    refactor part 3

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import connect
-# from datetime import datetime, timedelta, date
-# from twilio.rest import Client
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -28,12 +27,16 @@ yesterday_closing_price = float(yesterday_data["4. close"])
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = float(day_before_yesterday_data["4. close"])
 
-difference = abs(yesterday_closing_price - day_before_yesterday_closing_price)
-diff_percent = difference / yesterday_closing_price * 100
-
-if diff_percent > 5:
-    pass
+difference = yesterday_closing_price - day_before_yesterday_closing_price
+up_down = None
+if difference > 0:
+    up_down = "🔺"
 else:
+    up_down = "🔻"
+
+diff_percent = round(difference / yesterday_closing_price * 100)
+
+if abs(diff_percent) > .5:
     news_parameters = {
         "qInTitle": COMPANY_NAME,
         "apiKey": connect.NEWS_API_KEY
@@ -42,16 +45,16 @@ else:
     news_response = requests.get(NEWS_ENDPOINT, params=news_parameters)
     articles = news_response.json()["articles"]
     three_articles = articles[:3]
-    print(three_articles)
 
-# client = Client(connect.ACCOUNT_SID, connect.AUTH_TOKEN)
+    formatted_articles = [f"{COMPANY_NAME}: {up_down}{diff_percent}%\n"
+                          f"Headline: {article['title']} \n"
+                          f"Brief: {article['description']}" for article in articles]
 
-# for n in range(3):
-#     text_body = f"{STOCK}"
-#     print(STOCK)
-#     print(str(f"{round(stock_change)}%"))
-#     print(news_data["articles"][n]["title"])
-#     print(news_data["articles"][n]["description"])
+    client = Client(connect.TWILIO_SID, connect.TWILIO_AUTH_TOKEN)
 
-    # message = client.messages \
-    #     .create(body="Test", from_='+18662711651', to='+17143231961')
+    # for article in articles:
+    #     message = client.messages.create(
+    #         body=article,
+    #         from_='+18662711651',
+    #         to='+17143231961'
+    #     )
